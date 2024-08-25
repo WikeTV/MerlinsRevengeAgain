c90a4b7f6644e1abf34ac5f2b68f753280045d64
WikeTV <thewikeonline@gmail.com>
2024-08-24 21:00:56 -0400

    Toggle Dev mode off when URL does not indicate "localhost" as the domain

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { getGameManager } from "./scenes/gameManager.js";
 
-const DEV_MODE = true;
+// Controls debug displays and other development-specific features.
+const DEV_MODE = window?.location?.href?.includes("localhost") ?? false; // dev mode on while accessed via localhost URL
 
 window.addEventListener("load", async () => {
     const { loop } = await getGameManager({ isDevMode: DEV_MODE });
