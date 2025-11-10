38b6c3f7cd9bdb2f344ccdf10481eee372b9922b
wike <thewikeonline@gmail.com>
2025-11-10 03:15:34 -0500

    add note to self

--- src/scenes/gameManager.js
+++ src/scenes/gameManager.js
@@ -205,6 +205,7 @@ const gameModes = {
             }
 
             if (lastState.canTransition && !lastState.isTransitionArrowsDrawn) {
+                //! This seems to always draw the arrows for the starting scene... need to track down why
                 nextGameManagers.sceneManager.drawTransitionArrows();
                 nextGameState.isTransitionArrowsDrawn = true;
             }
